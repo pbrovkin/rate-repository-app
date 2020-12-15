75544f44cb29aa31f2f75432557f274a9ff45d7f
pbrovkin <pbrovkin@gmail.com>
2020-12-15 18:12:44 +0200

    the reviewed repositories list

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Starts: {item.stargazersCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+    </View>
+  );
+}
+
+export default RepositoryItem;
