c06e467b31d71c6cc8329db003ca9e2a569e7a20
pbrovkin <pbrovkin@gmail.com>
2020-12-25 12:54:48 +0200

    environment variables

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,10 @@
 import ApolloClient from 'apollo-boost';
 
+import Constants from 'expo-constants';
+
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.1.123:5000/graphql',
+    uri: Constants.manifest.extra.apolloUri,
   });
 };
 
