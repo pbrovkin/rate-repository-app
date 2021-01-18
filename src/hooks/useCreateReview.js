ee698080a53b4e4e195b7f7c7a8032a1bcfcc870
pbrovkin <pbrovkin@gmail.com>
2021-01-18 17:16:57 +0200

    review form

--- src/hooks/useCreateReview.js
+++ src/hooks/useCreateReview.js
@@ -1,19 +1,20 @@
 import { useMutation } from '@apollo/react-hooks';
 import { useHistory } from 'react-router-native';
 
-import { CREATE_REVIEW } from '../qraphql/mutations'
+import { CREATE_REVIEW } from '../qraphql/mutations';
 
 const useCreateReview = () => {
   const [createReview] = useMutation(CREATE_REVIEW);
-
   const history = useHistory();
-  
-  const create = async (values) => {
+
+  const addReview = async (values) => {
     const { data } = await createReview({ variables: { ...values, rating: Number(values.rating) } });
-    history.push(`/repositories/${data.createReview.repositoryId}`);
+    if (data) {
+      history.push(`/repositories/${data.createReview.repositoryId}`);
+    }
   };
 
-  return create;
+  return addReview;
 };
 
 export default useCreateReview;
